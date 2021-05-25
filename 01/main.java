6030b1198611f744cb92b8867ad60457a83f8fb7
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2021-05-25 20:45:37 +0200

    adding exercise 1

---
+++ 01/main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+    }
+    // void means that nothing will be returned using this method
+    // main means > entry point of the application
+}
+
+
+
